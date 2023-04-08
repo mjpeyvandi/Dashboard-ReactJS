7b8c920116fb216a2671f4e898c51de516c41358
mjpeyvandi <mohammadjp12@gmail.com>
2023-04-08 21:40:44 +0330

    search task

--- dashboard/src/components/Sidebar section/sidebar.jsx
+++ dashboard/src/components/Sidebar section/sidebar.jsx
@@ -74,6 +74,17 @@ const Sidebar = ()=>{
                         
                     </li>
 
+                    <li className="listItem">
+                        <Link to="/search" className="menuLink flex">
+                            <span className="smallText">
+                             جستجو محصول
+                            </span>
+                            <MdDeliveryDining className="icon"/>
+
+                        </Link>
+                        
+                    </li>
+
                     
                 </ul>
             </div>
